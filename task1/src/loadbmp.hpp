461cfc5ba196ffe4b605988ae16ba98fe6a7259b
artyrian <artyrian@ya.ru>
2011-09-14 18:28:56 +0400

    Delete old src/

--- task1/src/loadbmp.hpp
+++
@@ -1,6 +0,0 @@
-#ifndef _LOADBMP_HPP_
-#define _LOADBMP_HPP_
-
-unsigned char *LoadTrueColorBMPFile (const char *, int *, int *);
-
-#endif
--- task1/src/main.cpp
+++
@@ -1,6 +0,0 @@
-#include "loadbmp.hpp"
-
-int main ()
-{
-	return 0;
-}
